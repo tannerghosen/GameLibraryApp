5518e5cfa00e59c8246e68ba7e2a98368f5619a0
Tanner Ghosen <tannerghosen@yahoo.com>
2025-02-24 03:45:14 -0500

    Got rid of unused code for an OAuth login in GLAHttpClient until I actually add more platforms, which would be eventually

--- GLAHttpClient.cs
+++ GLAHttpClient.cs
@@ -1,16 +1,5 @@
-﻿using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Text;
-using System.Text.Json;
-using System.Threading.Tasks;
-
-namespace GamesLibraryApp
+﻿namespace GamesLibraryApp
 {
-    public class TokenResponse
-    {
-        public string RefreshToken { get; set; }
-        public string AccessToken { get; set; }
-    }
     public static class GLAHttpClient
     {
         private static readonly HttpClient hc = new HttpClient();
@@ -42,25 +31,6 @@ namespace GamesLibraryApp
                 Console.Error.WriteLine("ERROR: IsUrlUp: Failed to connect to the url at " + url + " .");
                 return false;
                 
-            }
-        }
-
-        // Mockup code to get access into some APIs via OAuth.
-        public static async Task<(string, string)> Login(string url, string username, string password)
-        {
-            try
-            {
-                var response = await hc.PostAsync(url, new StringContent(JsonSerializer.Serialize(new { username = username, password = password }), Encoding.UTF8, "application/json"));
-                if (response.IsSuccessStatusCode)
-                {
-                    var data = JsonSerializer.Deserialize<TokenResponse>(await response.Content.ReadAsStringAsync());
-                    return (data.RefreshToken, data.AccessToken);
-                }
-                return (null, null);
-            }
-            catch
-            {
-                return (null, null);
             }
         }
 
